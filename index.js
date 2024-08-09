5d9cfb70d0568ea19a74416673868f2a9fedc37c
aniket handore <aniket.handore@projectx38.com>
2024-08-10 00:01:39 +0530

    port no change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 
 app.use(cors());
 app.use(express.json());
